bd09065a6fcc3bad6cf503dc81927ae0f3ceafee
shaffer-wv <shaffer-wv@outlook.com>
2013-08-10 16:03:33 -0400

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,59 +1,35 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "has the content 'Sample App'" do
-    	visit '/static_pages/home'
-    	page.should have_content('Sample App')
-    end
-    
-    it "has the base title" do
-    	visit '/static_pages/home'
-    	page.should have_title("Ruby on Rails Tutorial Sample App")
-    end    
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-  	it "has the content 'Help'" do
-  		visit '/static_pages/help'
-  		page.should have_content('Help')
-  	end
-
-  	it "has the right title" do
-  		visit  '/static_pages/help'
-  		page.should have_title("#{base_title} | Help")
-  	end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-  	it "has the content 'About Us'" do
-  		visit '/static_pages/about'
-  		page.should have_content('About Us')
-    end
-
-    it "has the right title" do
-    	visit '/static_pages/about'
-    	page.should have_title("#{base_title} | About")
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "has the content 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_content('Contact')
-    end
-
-    it "has the right title" do
-      visit '/static_pages/contact'
-      page.should have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
-
 end
